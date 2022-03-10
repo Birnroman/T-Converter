cd1daff89031dfcfc4b2f48c531076246f87cfe9
Rum Gersy <angersy@ya.ru>
2022-03-10 09:45:23 +0300

    New project

--- T-Converter/ViewController.swift
+++ T-Converter/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var fahrenheitLabel: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = 0
+        fahrenheitLabel.text = "32ºF"
     }
 
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        
+        let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+    }
+    
+    
 
+    
 }
+
 
